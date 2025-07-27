0996f526c668eb2dba4e00a61cb67d46f186d456
E-Liyon Design <145768469+E-LyonDev@users.noreply.github.com>
2025-07-27 11:06:26 +0100

    2. Setting up our landing page with Imperative routing

--- alx-project-0x03/pages/_app.tsx
+++ alx-project-0x03/pages/_app.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "../styles/globals.css"; // 👈 This connects your CSS!
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
+
+export default App;
--- alx-project-0x03/tailwind.config.js
+++ alx-project-0x03/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Add more paths if needed
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
