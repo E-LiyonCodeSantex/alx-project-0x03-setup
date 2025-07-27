aabb2b435e181825fec30d36c714e45a89bd6b82
E-Liyon Design <145768469+E-LyonDev@users.noreply.github.com>
2025-07-27 12:39:19 +0100

    3. Move your interfaces to the right directory

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -5,7 +5,7 @@ import { PageRouteProps } from "@/interface/index"
 
 
 export default function Home() {
-  const router = useRouter ();
+  const router = useRouter();
 
   // Imeperative routing with useRouter
   const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
